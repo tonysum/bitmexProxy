7d772d64a838093997d40f53de881eb9f02c6aa5
tonysum <tonysum@126.com>
2020-05-28 16:23:11 +0800

    modify main.go add client.html

--- main.go
+++ main.go
@@ -2,20 +2,73 @@ package main
 
 import (
 	"net/http"
-
+	"log"
 	"fmt"
+	"github.com/gorilla/websocket"
 )
 
-const msg string = "Hello Gopher's World!"
-func MainProc(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte(msg))
+var upGrade = websocket.Upgrader{
+	ReadBufferSize:1024,
+	WriteBufferSize:1024,
 }
-func main()  {
-	proxyServer := http.NewServeMux()
-	proxyServer.HandleFunc("/",MainProc)
-	//http.HandleFunc("/",MainProc)
-	err := http.ListenAndServe(":8090", proxyServer)
-	if err != nil {
-		fmt.Println("ok")
+
+const message  = "Hello Gopher's World!"
+const wsmessage  = "Hello webSocket is ok!"
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w,message)
+	w.Header().Set("content-type","text/plain; charset=uft-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(message))
+}
+
+func reader(conn *websocket.Conn)  {
+	for{
+		messageType, p ,err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType,p); err != nil {
+			log.Println(err)
+			return
+		}
 	}
+}
+/*
+func writer(conn *websocket.Conn)  {
+	var str []byte
+	str =[]byte("Welcome my VIP!")
+	err := conn.WriteMessage(1,str)
+	if err != nil{
+		log.Fatal("Write Message Error: ", err)
+	}
+}
+*/
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upGrade.CheckOrigin = func(r *http.Request) bool {return true}
+	ws, err := upGrade.Upgrade(w,r,nil)
+	if err != nil {
+		fmt.Println(err)
+	
+	}
+	fmt.Println("Clinet Successfully connected ...")
+	reader(ws)
+	//writer(ws)
+
+
+}
+
+
+
+func main()  {
+	fmt.Println("Go WebScoket Server is running ... ")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",homePage)
+	mux.HandleFunc("/ws",wsEndpoint)
+	log.Fatal(http.ListenAndServe(":80", mux))
+
 }
